f2aa29396a2fc41df3fefe3094570ebd2d9576ed
Eremin Daniil <eremin.address@gmail.com>
2022-05-06 21:12:38 +0300

    !

--- src/main/java/ru/eremin/project/configs/WebSecurityConfig.java
+++ src/main/java/ru/eremin/project/configs/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import ru.eremin.project.services.UserService;
 
 @Configuration
@@ -39,7 +40,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("login")
                 .successHandler(successUserHandler);
 
-        http.logout().permitAll();
+        //Обрабатываем выход из приложения
+        http.logout()
+                .logoutSuccessUrl("/logout");
     }
 
     @Bean
--- src/main/java/ru/eremin/project/controllers/AuthorizationController.java
+++ src/main/java/ru/eremin/project/controllers/AuthorizationController.java
@@ -1,6 +1,9 @@
 package ru.eremin.project.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,8 @@ import ru.eremin.project.models.User;
 import ru.eremin.project.services.RoleService;
 import ru.eremin.project.services.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Set;
 
 @Controller
@@ -48,5 +53,14 @@ public class AuthorizationController {
         user.setRoles(role);
         userService.saveUser(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
